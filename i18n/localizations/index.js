e3dc8e99324c7b3ef1545156f9eab53c2bda64f5
Andrew Monks <andrew@theuprisingcreative.com>
2016-10-24 19:43:32 -0500

    removed sonos i18n

--- i18n/localizations/index.js
+++
@@ -1,6 +0,0 @@
-import en_us from './en_us' // eslint-disable-line
-
-export default Object.freeze({
-  en_us
-})
-
--- i18n/text.js
+++
@@ -1,5 +0,0 @@
-import __ from './helper'
-
-export default locale => ({path, args = []}) =>
-  <span dangerouslySetInnerHTML={{__html: __(locale)(path, args)}} />
-
