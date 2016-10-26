2cf823987360e9c1ac8ba2f43edcd728734fc809
Andrew Monks <andrew@theuprisingcreative.com>
2016-10-26 13:38:25 -0500

    remove unibabel dependency

--- util/base64/index.browser.js
+++ util/base64/index.browser.js
@@ -1,8 +1,19 @@
-import { compose } from 'ramda'
-import unibabel from 'unibabel'
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#Solution_1_–_escaping_the_string_before_encoding_it
 
-export const encode = unibabel.utf8ToBase64
-export const decode = unibabel.base64ToUtf8
+import { compose, join, map } from 'ramda'
+
+const decode = compose(
+  decodeURIComponent,
+  join(''),
+  map(c => `%${('00' + c.charCodeAt(0).toString(16)).slice(-2)}`),
+  window.atob
+)
+
+const encode = compose(
+  window.btoa,
+  str => str.replace(/%([0-9A-F]{2})/g, (match, p1) => String.fromCharCode(`0x${p1}`)),
+  encodeURIComponent
+)
 
 export const deserialize = compose(
   JSON.parse,
