135a944431a27e5f65d9626691e805142585d25d
Andrew Monks <andrew@theuprisingcreative.com>
2016-10-27 13:21:44 -0500

    test regexps

--- util/regex.js
+++ util/regex.js
@@ -1,3 +1,27 @@
-export const domain = new RegExp(/\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b)/)
-export const email = new RegExp(/([a-z0-9!#$%&'*+\/=?\^_`{|}~\-]+)@/ + domain.source)
+const wrap = regexp => new RegExp('^' + regexp.source + '$')
+
+const _domain = new RegExp(/((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}/)
+export const domain = wrap(_domain)
+
+export const email = wrap((() => {
+  const user = new RegExp(/([a-z0-9!#$%&'*+\/=?\^_`{|}~\-]+)/)
+  return new RegExp(user.source + '@' + _domain.source)
+})())
+
+// // this is kinda like testing...
+// //
+// const test = (expectation, str, regexp) => {
+//   const result = regexp.test(str)
+//   const msg = result === expectation
+//     ? '👍' : '👎'
+//   console.log(msg, str, regexp.test(str))
+// }
+
+// test(true, 'a+b@monks.co', email)
+// test(false, 'a@mo+nks.co', email)
+// test(false, '$%$ a @monks.co', email)
+// test(true, 'monks.co', domain)
+// test(true, 'xn-onks-99b.com', domain)
+
+// console.log('done')
 
