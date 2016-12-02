0bba03a106854d44054154b1cc6cbb76f6de1366
Andrew Monks <a@monks.co>
2016-12-02 00:30:44 -0600

    adds fn.noRepeat

--- src/fn.js
+++ src/fn.js
@@ -1,8 +1,10 @@
 // @flow
 
-import { curry } from 'ramda'
+import { curry, equals } from 'ramda'
 
 /**
+ * @name fn.iterate
+ * @desc
  * repeat a function n times, invoking it on its result
  * @example
  * const add5 = iterate(5, add(1))
@@ -17,4 +19,29 @@ const _iterate = (n: number, fn: Function, val: any): any => {
   return v
 }
 export const iterate = curry(_iterate)
+
+/**
+ * @name fn.noRepeat
+ * @desc
+ * cross between debounce and memoize
+ *
+ * I'm not sure how to explain this, look at the example
+ *
+ * @example
+ * const arr = []
+ * const once = noRepeat(val => { arr.push(val); return arr })
+ * once(1) // -> [1]
+ * once(1) // -> [1]
+ * once(2) // -> [1, 2]
+ * once(2) // -> [1, 2]
+ * once(1) // -> [1, 2, 1]
+ */
+export const noRepeat = (fn: Function): Function => {
+  let prev
+  return (...args: any): void => {
+    if (equals(args, prev)) return
+    prev = args
+    fn(...args)
+  }
+}
 
--- src/fn.test.js
+++ src/fn.test.js
@@ -8,3 +8,19 @@ test('iterates correctly', () => {
   expect(add5(0)).toBe(5)
 })
 
+test('noRepeats correctly', () => {
+  const fn = require('./fn')
+  const arr = []
+  const once = fn.noRepeat(val => { arr.push(val); return arr })
+  once(1)
+  expect(arr).toEqual([1])
+  once(1)
+  expect(arr).toEqual([1])
+  once(2)
+  expect(arr).toEqual([1, 2])
+  once(2)
+  expect(arr).toEqual([1, 2])
+  once(1)
+  expect(arr).toEqual([1, 2, 1])
+})
+
